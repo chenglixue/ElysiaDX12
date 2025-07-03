b577e0883322a255f48c65fc09b0e759b1f50e5c
Elysia <Elysia@qq.com>
2025-07-03 18:49:42 +0800

    Constant Buffer

---
+++ ElysiaRenderer/DX12ConstantBuffer.cpp
@@ -0,0 +1,23 @@
+#include"DX12ConstantBuffer.h"
+
+namespace ElysiaRenderer
+{
+	DX12ConstantBuffer::DX12ConstantBuffer(ID3D12Resource* resource, D3D12_RESOURCE_STATES usageState, uint32_t bufferSize)
+		: DX12GPUResource(resource, usageState)
+	{
+		m_bufferSize = bufferSize;
+		m_mappedBuffer = nullptr;
+		m_resource->Map(0, nullptr, &m_mappedBuffer);
+	}
+
+	DX12ConstantBuffer::~DX12ConstantBuffer()
+	{
+		m_resource->Unmap(0, nullptr);
+	}
+
+	void DX12ConstantBuffer::SetConstantBufferData(const void* bufferData, uint32_t bufferSize)
+	{
+		assert(bufferSize <= m_bufferSize);
+		memcpy(m_mappedBuffer, bufferData, bufferSize);
+	}
+}
--- ElysiaRenderer/DX12ConstantBuffer.h
+++ ElysiaRenderer/DX12ConstantBuffer.h
@@ -7,11 +7,14 @@ namespace ElysiaRenderer
 	class DX12ConstantBuffer : public DX12GPUResource
 	{
 	public:
-		DX12ConstantBuffer(ID3D12Resource* resource, D3D12_RESOURCE_STATES usageState, uint32_t bufferSize, 
-			DescriptorHeapHandle);
+		DX12ConstantBuffer(ID3D12Resource* resource, D3D12_RESOURCE_STATES usageState, uint32_t bufferSize);
 		~DX12ConstantBuffer() override;
 
+		void SetConstantBufferData(const void* bufferData, uint32_t bufferSize);
+
 	private:
+		void* m_mappedBuffer;
+		uint32_t m_bufferSize;
 
 	};
 }
